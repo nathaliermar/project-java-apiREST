3fbf821417d412c86f705ca3b8d21451db82f6ec
nathaliermar <nathalie.rmartines@gmail.com>
2023-10-22 18:41:37 -0300

    First publication Railway

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java - jar build/libs/project-java-apiREST-0.0.1-SNAPSHOT.jar
---
+++ src/main/java/me/dio/service/impl/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package me.dio.service.impl;
+
+import me.dio.domain.model.User;
+import me.dio.domain.model.repository.UserRepository;
+import me.dio.service.UserService;
+
+import java.util.NoSuchElementException;
+
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User userToCreate) {
+        if (userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())) {
+            throw new IllegalArgumentException("This Account number already exists.");
+        }
+        return userRepository.save(userToCreate);
+    }
+}
